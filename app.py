79a3152ac50f38901f8abbabcbbd88c9bb368ef2
Aldenivan Jr <aldenivan1207@gmail.com>
2022-01-11 11:10:14 -0300

    feat: creating two routes

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
 # Código do dev aqui
+from flask import Flask
+from datetime import datetime
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return {"data": "Hello flask"}
+
+
+@app.route("/current_datetime")
+def date_time():
+    
+    now = datetime.now()
+    message = ""
+
+    if(now.strftime("%P") == "am"):
+        message = "Bom dia!"
+    elif(now.strftime("%I") >= "6" and now.strftime("%P") == "pm"):
+        message = "Boa noite!"
+    else:
+        message - "Boa tarde!"    
+
+    return { "current_datetime": now.strftime("%d/%m/%y %H:%M:%S"), "message": message } 
